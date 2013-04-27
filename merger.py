804f3d9b550017d867b3d8e5f3807b3c650ced5f
JT Huang <jthuang@ischool.berkeley.edu>
2013-04-27 13:51:03 -0700

    Fixed that the merger may only print the partial attributes.

--- merger.py
+++ merger.py
@@ -33,6 +33,8 @@ merged_json_file = open(MERGED_JSON, "w")
 merged_item_cnt = 0
 youtube_item_cnt = 0
 nomatch_cnt = 0
+losing_cnt = {'lang': 0, 'topic': 0, 'event': 0, 'country': 0}
+losing_attr_cnt = 0
 with open(YOUTUBE_JSON, "r") as youtube_json_file:
     for line in youtube_json_file:
         youtube_item_cnt += 1
@@ -48,10 +50,16 @@ with open(YOUTUBE_JSON, "r") as youtube_json_file:
             for attr in SITE_ATTR_LIST:
                 if attr in video_dict[vid]:
                     merged_video['data'][attr] = video_dict[vid][attr]
-                    print(json.dumps(merged_video), file=merged_json_file)
+                else:
+                    print("losing attr %s of %s" % (attr, vid))
+                    losing_cnt[attr] += 1
+                    losing_attr_cnt += 1
+            print(json.dumps(merged_video), file=merged_json_file)
             merged_item_cnt += 1
 
 youtube_json_file.close()
 merged_json_file.close()
 
-print("site: %d, YouTube: %d, nomatch: %d, merged: %d" % (len(video_dict), youtube_item_cnt, nomatch_cnt, merged_item_cnt))
+print("site: %d, YouTube: %d, nomatch: %d, merged: %d (losing_cnt: %d)" % (len(video_dict), youtube_item_cnt, nomatch_cnt, merged_item_cnt, losing_attr_cnt))
+for attr in SITE_ATTR_LIST:
+    print("losing %s: %d" % (attr, losing_cnt[attr]))
